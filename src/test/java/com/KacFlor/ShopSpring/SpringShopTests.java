c488eb6d265e089e636ba5406540145dde790c60
KacFlor <kacper.florczyk@onet.pl>
2024-04-22 13:53:46 +0200

    CI-workflow-fix-try-10

--- src/test/java/com/KacFlor/ShopSpring/SpringShopTests.java
+++ src/test/java/com/KacFlor/ShopSpring/SpringShopTests.java
@@ -1,13 +1,8 @@
 package com.KacFlor.ShopSpring;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringShopTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
