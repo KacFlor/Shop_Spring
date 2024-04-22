c09d3c257d876cbf81f1ffeec7fed3b87086f90b
KacFlor <kacper.florczyk@onet.pl>
2024-04-22 17:17:56 +0200

    added role column to User, try to write security

--- src/main/java/com/KacFlor/ShopSpring/config/SecurityConfig.java
+++ src/main/java/com/KacFlor/ShopSpring/config/SecurityConfig.java
@@ -1,5 +1,24 @@
 package com.KacFlor.ShopSpring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/cart").hasRole("ADMIN")
+                                .requestMatchers("/carddata").hasRole("USER")
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
