52fa7572c4b781110e1d908421f87eb73404fffd
KacFlor <kacper.florczyk@onet.pl>
2024-05-07 21:38:52 +0200

    endpoints and logic for Category

---
+++ src/main/java/com/KacFlor/ShopSpring/controllersRequests/NewCategory.java
@@ -0,0 +1,17 @@
+package com.KacFlor.ShopSpring.controllersRequests;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewCategory{
+
+    private String name;
+
+}
