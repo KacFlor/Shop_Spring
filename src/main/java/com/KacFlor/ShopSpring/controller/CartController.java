d1e7966d6d9d508ca8ecd33139a7a89ce87818b8
KacFlor <kacper.florczyk@onet.pl>
2024-05-08 21:50:24 +0200

    logic and endpoint for cart done

--- src/main/java/com/KacFlor/ShopSpring/controller/CartController.java
+++ src/main/java/com/KacFlor/ShopSpring/controller/CartController.java
@@ -1,17 +1,24 @@
 package com.KacFlor.ShopSpring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.KacFlor.ShopSpring.model.Category;
+import com.KacFlor.ShopSpring.model.Role;
+import com.KacFlor.ShopSpring.model.Shipment;
 import com.KacFlor.ShopSpring.service.CartService;
 import com.KacFlor.ShopSpring.model.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(
-        path = {"cart"}
+        path = {"carts"}
 )
 public class CartController{
 
@@ -22,8 +29,23 @@ public class CartController{
         this.cartService = cartService;
     }
 
+    @PreAuthorize("hasAuthority('" + Role.Fields.ADMIN + "')")
     @GetMapping
-    public List<Cart> getCart(){
-        return this.cartService.getCart();
+    public List<Cart> getAll(){
+        return this.cartService.getAll();
+    }
+
+    @PreAuthorize("hasAuthority('" + Role.Fields.ADMIN + "')")
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Cart>> getById(@PathVariable("id") Integer id){
+        Optional<Cart> cart = Optional.ofNullable(cartService.getById(id));
+        return ResponseEntity.ok(cart);
+    }
+
+    @PreAuthorize("hasAnyAuthority('" + Role.Fields.USER + "', '" + Role.Fields.ADMIN + "')")
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Optional<Cart>> getCustomerCart(@PathVariable("id") Integer id){
+        Optional<Cart> cart = Optional.ofNullable(cartService.getCartByCustomerId(id));
+        return ResponseEntity.ok(cart);
     }
 }
--- src/main/java/com/KacFlor/ShopSpring/service/CartService.java
+++ src/main/java/com/KacFlor/ShopSpring/service/CartService.java
@@ -1,23 +1,54 @@
 package com.KacFlor.ShopSpring.service;
 
+import com.KacFlor.ShopSpring.config.ExceptionsConfig;
+import com.KacFlor.ShopSpring.dao.CustomerRepository;
 import com.KacFlor.ShopSpring.model.Cart;
 import com.KacFlor.ShopSpring.dao.CartRepository;
+import com.KacFlor.ShopSpring.model.Category;
+import com.KacFlor.ShopSpring.model.Customer;
+import com.KacFlor.ShopSpring.model.Shipment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService{
 
     private final CartRepository cartRepository;
 
+    private final CustomerRepository customerRepository;
+
     @Autowired
-    public CartService(CartRepository cartRepository){
+    public CartService(CartRepository cartRepository, CustomerRepository customerRepository){
         this.cartRepository = cartRepository;
+        this.customerRepository = customerRepository;
     }
 
-    public List<Cart> getCart(){
+    public List<Cart> getAll(){
         return cartRepository.findAll();
+    }
+
+    public Cart getById(Integer Id){
+
+        Optional<Cart> optionalCart = cartRepository.findById(Id);
+        if(optionalCart.isPresent()){
+            return optionalCart.get();
+        }
+        else{
+            throw new ExceptionsConfig.ResourceNotFoundException("Cart not found");
+        }
+    }
+
+    public Cart getCartByCustomerId(Integer Id){
+
+        Optional<Customer> customer = customerRepository.findById(Id);
+        if(customer.isPresent()){
+            return cartRepository.findByCustomerId(Id);
+        }
+        else{
+            throw new ExceptionsConfig.ResourceNotFoundException("Customer not found");
+        }
     }
 }
